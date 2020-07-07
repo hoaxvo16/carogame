2a011d8370e18cfa4fc1576c1ebb87baf4ed7dd3
VoHoa-HCMUS <vohoa1468@gmail.com>
2020-07-07 10:15:48 +0700

    update event click

--- public/index.js
+++ public/index.js
@@ -1,8 +1,22 @@
+//kết nối tới server
 var socket = io("http://localhost:3000");
+//Xử lý những gì client gửi cho server
 $(document).ready(function () {
-  for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < 10; j++) {
-      $("#box-content").append("<rec class='box-inside'>x</rec>");
+  let width = $("#box-content").width();
+  let height = $("#box-content").height();
+  for (let i = 0; i < width / 20; i++) {
+    for (let j = 0; j < height / 20; j++) {
+      $("#box-content").append(
+        "<rec class='box-inside' id=" +
+          i.toString() +
+          "+" +
+          j.toString() +
+          ">x</rec>"
+      );
     }
   }
+  $(".box-inside").click(function (event) {
+    alert(event.target.id);
+  });
 });
+//Xử lý những gì server gửi cho client
--- server.js
+++ server.js
@@ -1,20 +1,22 @@
+//require thư viện
 var express = require("express");
 var app = express();
-
+//set use: những file gửi cho client, view engine: loại file hiển thị, thư mục chứa file view
 app.use(express.static("./public"));
 app.set("view engine", "ejs");
 app.set("views", "./views");
-
+//require http va socket io
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
 
 server.listen(process.env.PORT || 3000);
 
 console.log("server is running");
-
+//Có người kết nối
 io.on("connection", function (socket) {
   console.log("Connected with: " + socket.id);
 });
+//Render file ejs
 app.get("/", function (req, res) {
   res.render("index");
 });
