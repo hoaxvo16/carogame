25a8d7d4cbecb08d299ed24e6ef0c0d1bdad8a4a
Hoa-Vo <vohoa1468@gmail.com>
2020-07-09 14:18:35 +0700

    add join room and send username

--- public/index.js
+++ public/index.js
@@ -2,21 +2,62 @@
 var socket = io("http://localhost:3000");
 //Xử lý những gì client gửi cho server
 $(document).ready(function () {
+  $("#login-form").show();
+  $("#room-list").show();
+  $("#box-content").hide();
+  $("#current-room").hide();
+  // let width = $("#box-content").width();
+  // let height = $("#box-content").height();
+  // for (let i = 0; i < width / 20; i++) {
+  //   for (let j = 0; j < height / 20; j++) {
+  //     $("#box-content").append(
+  //       "<rect class='box-inside' id=" +
+  //         i.toString() +
+  //         "+" +
+  //         j.toString() +
+  //         "></rect>"
+  //     );
+  //   }
+  // $(".box-inside").click(function (event) {;
+  //   $(this).css("color", "blue");
+  //   $(this).html("X");
+  // });
+  $("#btn").click(function (data) {
+    socket.emit("client-send-user-name", $("#username").val());
+  });
+});
+//Xử lý những gì server gửi cho client
+socket.on("server-send-roomlist", function (data) {
+  $("#room-list").html("");
+  data.forEach(element => {
+    console.log(element.name);
+    $("#room-list").append("<div class='room'>" + element.name + "</div>");
+  });
+});
+
+socket.on("user-join-room", function (roomName, user) {
+  $("#login-form").hide();
+  $("#current-room").show();
+  $("#current-room").html(roomName);
+  console.log(user);
+  $("#user-login").append("<div>" + user.name + "</div>");
+  $("#box-content").show();
+  $("#room-list").hide();
   let width = $("#box-content").width();
   let height = $("#box-content").height();
   for (let i = 0; i < width / 20; i++) {
     for (let j = 0; j < height / 20; j++) {
       $("#box-content").append(
-        "<rec class='box-inside' id=" +
+        "<rect class='box-inside' id=" +
           i.toString() +
           "+" +
           j.toString() +
-          ">x</rec>"
+          "></rect>"
       );
     }
   }
   $(".box-inside").click(function (event) {
-    alert(event.target.id);
+    $(this).css("color", "blue");
+    $(this).html("X");
   });
 });
-//Xử lý những gì server gửi cho client
--- server.js
+++ server.js
@@ -10,11 +10,92 @@ var server = require("http").Server(app);
 var io = require("socket.io")(server);
 
 server.listen(process.env.PORT || 3000);
-
+class user {
+  constructor(name, type) {
+    this.name = name;
+    this.type = type;
+  }
+}
+var rooms = [];
+class room {
+  constructor(name, numuser) {
+    this.name = name;
+    this.numOfUser = numuser;
+    this.userArr = [];
+  }
+  increaseNumOfUser() {
+    this.numOfUser++;
+  }
+  addUser(user) {
+    this.userArr.push(user);
+  }
+  subUser() {
+    this.numOfUser--;
+  }
+  getNumOfUser() {
+    return this.numOfUser;
+  }
+}
 console.log("server is running");
 //Có người kết nối
 io.on("connection", function (socket) {
   console.log("Connected with: " + socket.id);
+
+  io.sockets.emit("server-send-roomlist", rooms);
+  socket.on("client-send-user-name", function (data) {
+    let index = 1;
+    let check = false;
+    console.log("user " + socket.id + " send: " + data);
+    if (rooms.length) {
+      rooms.forEach(element => {
+        index++;
+        if (element.numOfUser < 2) {
+          currentUser = new user(data, 2);
+          socket.user = currentUser;
+          socket.room = element;
+          socket.join(socket.room);
+          element.addUser(currentUser);
+          element.increaseNumOfUser();
+          io.to(socket.room).emit("user-join-room", element.name, currentUser);
+          check = true;
+        }
+      });
+      if (!check) {
+        currentUser = new user(data, 1);
+        socket.user = currentUser;
+        currentRoom = new room("room " + index.toString(), 1);
+        currentRoom.addUser(currentUser);
+        rooms.push(currentRoom);
+        socket.room = currentRoom;
+        socket.join(socket.room);
+        io.to(socket.room).emit(
+          "user-join-room",
+          currentRoom.name,
+          currentUser
+        );
+      }
+    } else {
+      currentUser = new user(data, 1);
+      socket.user = currentUser;
+      currentRoom = new room("room 1", 1);
+      currentRoom.addUser(currentUser);
+      rooms.push(currentRoom);
+      socket.room = currentRoom;
+      socket.join(socket.room);
+      io.to(socket.room).emit("user-join-room", currentRoom.name, currentUser);
+    }
+  });
+  socket.on("disconnect", function () {
+    console.log(socket.id + " disconnected");
+    if (rooms.length) {
+      try {
+        socket.room.subUser();
+      } catch (error) {}
+    }
+    if (socket.room.getNumOfUser() === 0) {
+      rooms.splice(rooms.indexOf(socket.room), 1);
+    }
+  });
 });
 //Render file ejs
 app.get("/", function (req, res) {
